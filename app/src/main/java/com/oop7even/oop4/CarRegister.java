52a0dbe6cd6c0610d7056096e1828d7613672947
yymin1022 <yymin1022@gmail.com>
2022-12-05 23:57:32 +0900

    Added CarRegisterActivity

---
+++ app/src/main/java/com/oop7even/oop4/CarRegister.java
@@ -0,0 +1,13 @@
+package com.oop7even.oop4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarRegister extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_register);
+    }
+}
