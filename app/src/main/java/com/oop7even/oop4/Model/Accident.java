013f0f81e7d626a35897f00c1f787d692307655a
yymin1022 <yymin1022@gmail.com>
2022-12-06 03:07:13 +0900

    Update Accident Class : Migrated from Legacy

--- app/src/main/java/com/oop7even/oop4/Model/Accident.java
+++ app/src/main/java/com/oop7even/oop4/Model/Accident.java
@@ -1,4 +1,27 @@
 package com.oop7even.oop4.Model;
 
-public class Accident {
+public class Accident{
+    private String date;
+    private String content;
+
+    public Accident(String date, String content){
+        this.date = date;
+        this.content = content;
+    }
+
+    public String getDate(){
+        return this.date;
+    }
+
+    public String getContent(){
+        return this.content;
+    }
+
+    public void setDate(String date){
+        this.date = date;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
 }
