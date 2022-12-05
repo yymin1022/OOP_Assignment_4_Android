af76aa44c88ab8f3e14d2c557576a29a21c92d15
yymin1022 <yymin1022@gmail.com>
2022-12-05 23:58:40 +0900

    Added UserInfoActivity

---
+++ app/src/main/java/com/oop7even/oop4/UserInfo.java
@@ -0,0 +1,13 @@
+package com.oop7even.oop4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
