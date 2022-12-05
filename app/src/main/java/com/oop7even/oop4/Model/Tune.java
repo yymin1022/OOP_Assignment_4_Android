8cd66d5f249ffc21d509245ee57d6d6053ea27f7
yymin1022 <yymin1022@gmail.com>
2022-12-06 03:07:29 +0900

    Update Tune Class : Migrated from Legacy

--- app/src/main/java/com/oop7even/oop4/Model/Tune.java
+++ app/src/main/java/com/oop7even/oop4/Model/Tune.java
@@ -1,4 +1,27 @@
 package com.oop7even.oop4.Model;
 
-public class Tune {
+public class Tune{
+    private String date;
+    private String content;
+
+    public Tune(String date, String content){
+        this.date = date;
+        this.content = content;
+    }
+
+    public String getDate(){
+        return this.date;
+    }
+
+    public String getContent(){
+        return this.content;
+    }
+
+    public void setDate(String date){
+        this.date = date;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
 }
