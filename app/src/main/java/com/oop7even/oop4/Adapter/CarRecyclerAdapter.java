43b4564b27041fc822877e144b30415de7da7397
yymin1022 <yymin1022@gmail.com>
2022-12-07 16:06:01 +0900

    Update CarRecyclerAdapter : Added Car Info Variables

--- app/src/main/java/com/oop7even/oop4/Adapter/CarRecyclerAdapter.java
+++ app/src/main/java/com/oop7even/oop4/Adapter/CarRecyclerAdapter.java
@@ -34,8 +34,17 @@ public class CarRecyclerAdapter extends RecyclerView.Adapter<CarRecyclerAdapter.
 
     @Override
     public void onBindViewHolder(@NonNull CarRecyclerAdapter.ViewHolder holder, int position){
+        String carDist = String.valueOf(carData.get(position).getDistanceDriven());
+        String carPrice = String.valueOf(carData.get(position).getPrice());
+        String carYear = String.valueOf(carData.get(position).getYear());
+        String carColor = carData.get(position).getColor();
         String carName = carData.get(position).getName();
+
+        holder.tvCarColor.setText(carColor);
+        holder.tvCarDist.setText(carDist);
         holder.tvCarName.setText(carName);
+        holder.tvCarPrice.setText(carPrice);
+        holder.tvCarYear.setText(carYear);
     }
 
     @Override
@@ -44,12 +53,20 @@ public class CarRecyclerAdapter extends RecyclerView.Adapter<CarRecyclerAdapter.
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvCarColor;
+        TextView tvCarDist;
         TextView tvCarName;
+        TextView tvCarPrice;
+        TextView tvCarYear;
 
         ViewHolder(View itemView){
             super(itemView);
 
+            tvCarColor = itemView.findViewById(R.id.recycle_tv_color);
+            tvCarDist = itemView.findViewById(R.id.recycle_tv_dist);
             tvCarName = itemView.findViewById(R.id.recycle_tv_name);
+            tvCarPrice = itemView.findViewById(R.id.recycle_tv_price);
+            tvCarYear = itemView.findViewById(R.id.recycle_tv_year);
         }
     }
 }
