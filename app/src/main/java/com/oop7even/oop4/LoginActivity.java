6838cd9b5e96c99624694c4477d8c9e67c29bacd
yymin1022 <yymin1022@gmail.com>
2022-12-07 11:38:53 +0900

    Update LoginActivity : Cleaned UP

--- app/src/main/java/com/oop7even/oop4/LoginActivity.java
+++ app/src/main/java/com/oop7even/oop4/LoginActivity.java
@@ -14,7 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        completeLogin();
+//        completeLogin();
     }
 
     void completeLogin(){
