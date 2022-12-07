79a37528c2d479132fd05bbbeaada0049a694b70
yymin1022 <yymin1022@gmail.com>
2022-12-07 09:30:19 +0900

    Update MainActivity : Applied SupportActionBar

--- app/src/main/java/com/oop7even/oop4/MainActivity.java
+++ app/src/main/java/com/oop7even/oop4/MainActivity.java
@@ -1,6 +1,7 @@
 package com.oop7even.oop4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mainToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
     }
 }
