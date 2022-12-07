b59634ea858a698741f8bfabe51ba5fcdf4a75c5
yymin1022 <yymin1022@gmail.com>
2022-12-07 11:32:27 +0900

    Update MainActivity / LoginActivity : Added Login User Transaction

--- app/src/main/java/com/oop7even/oop4/LoginActivity.java
+++ app/src/main/java/com/oop7even/oop4/LoginActivity.java
@@ -2,12 +2,28 @@ package com.oop7even.oop4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.oop7even.oop4.Model.User;
+
 public class LoginActivity extends AppCompatActivity {
+    User user = new User("", false);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        user.setName("Sans");
+
+        completeLogin();
+    }
+
+    void completeLogin(){
+        Intent resultIntent = new Intent(this, LoginActivity.class);
+        resultIntent.putExtra("user", user);
+        setResult(9001, resultIntent);
+        finish();
     }
 }
--- app/src/main/java/com/oop7even/oop4/MainActivity.java
+++ app/src/main/java/com/oop7even/oop4/MainActivity.java
@@ -1,15 +1,21 @@
 package com.oop7even.oop4;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.oop7even.oop4.Model.User;
 
 public class MainActivity extends AppCompatActivity {
     boolean isLoggedIn = false;
+
+    ActivityResultLauncher<Intent> loginLauncher;
     User user = new User("", false);
 
     @Override
@@ -24,8 +30,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void openLogin(){
-        Intent intent = new Intent(this, LoginActivity.class);
-        intent.putExtra("user", user);
-        startActivity(intent);
+        Intent initIntent = new Intent(this, LoginActivity.class);
+        initIntent.putExtra("user", user);
+
+        loginLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result ->  {
+            if(result.getResultCode() == 9001){
+                Intent resultIntent = result.getData();
+
+                if(resultIntent != null){
+                    user = (User) resultIntent.getSerializableExtra("user");
+                    Toast.makeText(getApplicationContext(), user.getName(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        loginLauncher.launch(initIntent);
     }
 }
