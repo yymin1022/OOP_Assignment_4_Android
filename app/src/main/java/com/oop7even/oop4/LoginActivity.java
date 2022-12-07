5576b88b9a0587abd35220eb7280ebb162f92692
yymin1022 <yymin1022@gmail.com>
2022-12-07 13:41:21 +0900

    Update LoginActivity : Added Element Logic

--- app/src/main/java/com/oop7even/oop4/LoginActivity.java
+++ app/src/main/java/com/oop7even/oop4/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.oop7even.oop4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 import com.oop7even.oop4.Model.User;
 
@@ -14,7 +16,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-//        completeLogin();
+
+        AppCompatButton btnLogin = findViewById(R.id.login_btn_login);
+        CheckBox chkIsSeller = findViewById(R.id.login_chk_seller);
     }
 
     void completeLogin(){
