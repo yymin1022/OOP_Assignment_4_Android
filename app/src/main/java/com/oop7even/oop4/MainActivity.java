2c6a4466490510e50bee46408a506953d4e98927
yymin1022 <yymin1022@gmail.com>
2022-12-07 11:13:59 +0900

    Update MainActivity : Added Init Login Logic

--- app/src/main/java/com/oop7even/oop4/MainActivity.java
+++ app/src/main/java/com/oop7even/oop4/MainActivity.java
@@ -3,9 +3,15 @@ package com.oop7even.oop4;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.oop7even.oop4.Model.User;
+
 public class MainActivity extends AppCompatActivity {
+    boolean isLoggedIn = false;
+    User user = new User("", false);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar mainToolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
+
+        openLogin();
+    }
+
+    void openLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
     }
 }
