eff26a9f5d24c62a0f056d36505af3c22efa2fa6
yymin1022 <yymin1022@gmail.com>
2022-12-09 03:52:20 +0900

    Update CarDetailActivity : Added Car Data Transaction Logic

--- app/src/main/java/com/oop7even/oop4/CarDetailActivity.java
+++ app/src/main/java/com/oop7even/oop4/CarDetailActivity.java
@@ -3,11 +3,24 @@ package com.oop7even.oop4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.oop7even.oop4.Model.Car;
 
 public class CarDetailActivity extends AppCompatActivity{
     Car car;
+
+    Button btnBuyCar;
+    TextView tvCarCapacity;
+    TextView tvCarColor;
+    TextView tvCarDist;
+    TextView tvCarFuel;
+    TextView tvCarManufacture;
+    TextView tvCarName;
+    TextView tvCarNumber;
+    TextView tvCarPrice;
+    TextView tvCarYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -15,5 +28,30 @@ public class CarDetailActivity extends AppCompatActivity{
         setContentView(R.layout.activity_car_detail);
 
         car = (Car)getIntent().getSerializableExtra("Car");
+
+        btnBuyCar = findViewById(R.id.detail_btn_buy);
+        tvCarCapacity = findViewById(R.id.detail_tv_capacity);
+        tvCarColor = findViewById(R.id.detail_tv_color);
+        tvCarDist = findViewById(R.id.detail_tv_dist);
+        tvCarFuel = findViewById(R.id.detail_tv_fuel);
+        tvCarManufacture = findViewById(R.id.detail_tv_company);
+        tvCarNumber = findViewById(R.id.detail_tv_number);
+        tvCarName = findViewById(R.id.detail_tv_name);
+        tvCarPrice = findViewById(R.id.detail_tv_price);
+        tvCarYear = findViewById(R.id.detail_tv_year);
+
+        setCarInfo();
+    }
+
+    void setCarInfo(){
+        tvCarCapacity.setText(String.valueOf(car.getCapacity()));
+        tvCarColor.setText(car.getColor());
+        tvCarDist.setText(String.valueOf(car.getDistanceDriven()));
+        tvCarFuel.setText(car.getFuel());
+        tvCarManufacture.setText(car.getManufacture());
+        tvCarName.setText(car.getName());
+        tvCarNumber.setText(car.getNumber());
+        tvCarPrice.setText(String.valueOf(car.getPrice()));
+        tvCarYear.setText(String.valueOf(car.getYear()));
     }
 }
