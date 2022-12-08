7ae6b9b2465663fd007fbd54e88cc4a66cebecdb
yymin1022 <yymin1022@gmail.com>
2022-12-09 04:30:28 +0900

    Update MainActivity : Added Dummy Car Data

--- app/src/main/java/com/oop7even/oop4/MainActivity.java
+++ app/src/main/java/com/oop7even/oop4/MainActivity.java
@@ -79,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
         Car testCar = new Car("Veloster N", "Hyundai", "123구1234", "White", 2300, 5, 87120, 2019, "Gasoline", true, true);
         testCar.addAccident(new Accident("2022. 11. 28. 12:00", "차선변경 중 상대방 과실 100% 접촉사고. 좌측 휀더 교환"));
         testCar.addTune(new Tune("2022. 10. 23.", "배기구 2개, 가변밸브 1개"));
+        Car testCar2 = new Car("Elantra N", "Hyundai", "12사1123", "퍼포먼스 블루", 2600, 5, 32250, 2021, "Gasoline", false, false);
+        tmpCarList.add(testCar2);
         tmpCarList.add(testCar);
         tmpCarList.add(testCar);
         tmpCarList.add(testCar);
