be472041e685a5f430f791b61fcca8f59eb8fc7c
yymin1022 <yymin1022@gmail.com>
2022-12-10 05:11:19 +0900

    Update SearchResultActivity : Added RecyclerView Initialization Logic

--- app/src/main/java/com/oop7even/oop4/SearchResultActivity.java
+++ app/src/main/java/com/oop7even/oop4/SearchResultActivity.java
@@ -1,9 +1,13 @@
 package com.oop7even.oop4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.oop7even.oop4.Adapter.CarRecyclerAdapter;
 import com.oop7even.oop4.Model.Car;
 
 import java.util.ArrayList;
@@ -11,11 +15,28 @@ import java.util.ArrayList;
 public class SearchResultActivity extends AppCompatActivity{
     ArrayList<Car> carList;
 
+    RecyclerView recycleCar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
 
         carList = (ArrayList<Car>)getIntent().getSerializableExtra("car");
+
+        recycleCar = findViewById(R.id.result_recycler_car);
+        setupUI();
+    }
+
+    void setupUI(){
+        CarRecyclerAdapter carAdapter = new CarRecyclerAdapter(carList);
+        recycleCar.setAdapter(carAdapter);
+        recycleCar.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        carAdapter.setOnCarClickListener((position, clickedCar) -> {
+            Intent intent = new Intent(getApplicationContext(), CarDetailActivity.class);
+            intent.putExtra("Car", clickedCar);
+            startActivity(intent);
+        });
     }
 }
