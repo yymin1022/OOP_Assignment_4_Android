4719efa1a1f78c38b8f73da9421dbd90c8d286a4
yymin1022 <yymin1022@gmail.com>
2022-12-10 05:30:48 +0900

    Update UserInfoActivity : Added CarList / User Info Loading Logic from Intent

--- app/src/main/java/com/oop7even/oop4/UserInfoActivity.java
+++ app/src/main/java/com/oop7even/oop4/UserInfoActivity.java
@@ -4,10 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.oop7even.oop4.Model.Car;
+import com.oop7even.oop4.Model.User;
+
+import java.util.ArrayList;
+
 public class UserInfoActivity extends AppCompatActivity{
+    ArrayList<Car> carList;
+    User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        carList = (ArrayList<Car>)getIntent().getSerializableExtra("car");
+        user = (User)getIntent().getSerializableExtra("user");
     }
 }
