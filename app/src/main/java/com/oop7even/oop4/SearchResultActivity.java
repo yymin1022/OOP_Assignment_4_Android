a8e7648e907160d08d46daa0a07cc36f72e5d9fc
yymin1022 <yymin1022@gmail.com>
2022-12-10 05:06:33 +0900

    Update SearchResultActivity : Added ResultData Transaction Logic

--- app/src/main/java/com/oop7even/oop4/SearchResultActivity.java
+++ app/src/main/java/com/oop7even/oop4/SearchResultActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SearchResultActivity extends AppCompatActivity {
+import com.oop7even.oop4.Model.Car;
+
+import java.util.ArrayList;
+
+public class SearchResultActivity extends AppCompatActivity{
+    ArrayList<Car> carList;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+
+        carList = (ArrayList<Car>)getIntent().getSerializableExtra("car");
     }
 }
