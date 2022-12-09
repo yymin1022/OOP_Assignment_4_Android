2c37711db65a5657340f0f576dd2cad432e9632f
yymin1022 <yymin1022@gmail.com>
2022-12-10 04:05:28 +0900

    Update SearchActivity : Added Base Variables

--- app/src/main/java/com/oop7even/oop4/SearchActivity.java
+++ app/src/main/java/com/oop7even/oop4/SearchActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.android.material.slider.RangeSlider;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.oop7even.oop4.Model.Car;
 
 import java.util.ArrayList;
@@ -25,11 +30,31 @@ public class SearchActivity extends AppCompatActivity {
     String carModel;
     String carType;
 
+    EditText inputCompany;
+    EditText inputName;
+    MaterialButtonToggleGroup toggleType;
+    RadioGroup radioFuel;
+    RangeSlider sliderDistance;
+    RangeSlider sliderPrice;
+    RangeSlider sliderYear;
+    SwitchMaterial switchAccident;
+    SwitchMaterial switchTune;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
         carList = (ArrayList<Car>)getIntent().getSerializableExtra("car");
+
+        inputCompany = findViewById(R.id.search_input_company);
+        inputName = findViewById(R.id.search_input_name);
+        toggleType = findViewById(R.id.search_toggle_type);
+        radioFuel = findViewById(R.id.search_radio_fuel);
+        sliderDistance = findViewById(R.id.search_slider_distance);
+        sliderPrice = findViewById(R.id.search_slider_price);
+        sliderYear = findViewById(R.id.search_slider_year);
+        switchAccident = findViewById(R.id.search_switch_accident);
+        switchTune = findViewById(R.id.search_switch_tune);
     }
 }
