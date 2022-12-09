7b295165c20d4b32b9264fe96b8f8f394e47149f
yymin1022 <yymin1022@gmail.com>
2022-12-10 02:41:42 +0900

    Update SearchActivity : Added Car List Data Loading Logic

--- app/src/main/java/com/oop7even/oop4/SearchActivity.java
+++ app/src/main/java/com/oop7even/oop4/SearchActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.oop7even.oop4.Model.Car;
+
+import java.util.ArrayList;
+
 public class SearchActivity extends AppCompatActivity {
+    ArrayList<Car> carList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        carList = (ArrayList<Car>)getIntent().getSerializableExtra("car");
     }
 }
