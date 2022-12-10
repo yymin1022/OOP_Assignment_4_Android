1fbbd518c5765083e09ecfb3a1930aa341072f89
yymin1022 <yymin1022@gmail.com>
2022-12-11 01:58:54 +0900

    Update SearchActivity : Cleaned UP

--- app/src/main/java/com/oop7even/oop4/SearchActivity.java
+++ app/src/main/java/com/oop7even/oop4/SearchActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
