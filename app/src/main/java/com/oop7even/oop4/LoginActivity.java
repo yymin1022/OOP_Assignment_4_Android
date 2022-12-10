1bb4e066f0f604761c5a199c921841dd0c13986e
yymin1022 <yymin1022@gmail.com>
2022-12-11 00:55:42 +0900

    Update LoginActivity : Improved Firestore Runtime

--- app/src/main/java/com/oop7even/oop4/LoginActivity.java
+++ app/src/main/java/com/oop7even/oop4/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.oop7even.oop4;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
@@ -8,18 +7,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+import com.oop7even.oop4.Model.Accident;
+import com.oop7even.oop4.Model.Car;
+import com.oop7even.oop4.Model.Tune;
 import com.oop7even.oop4.Model.User;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class LoginActivity extends AppCompatActivity {
     boolean isSeller = false;
@@ -102,6 +105,68 @@ public class LoginActivity extends AppCompatActivity {
     };
 
     void completeLogin(){
+        ArrayList<Car> carList;
+        ArrayList<String> carNumberList = new ArrayList<>();
+
+        user.setIsSeller(true);
+        db.collection("User")
+                .get()
+                .addOnCompleteListener(task -> {
+                    boolean isLogin  = false;
+                    if(task.isSuccessful()){
+                        for(QueryDocumentSnapshot document : task.getResult()){
+                            if(document.getId().equals(userName)){
+                                isSeller = (boolean)document.getData().get("isSeller");
+
+                                for(String carNumber : (ArrayList<String>)document.getData().get("car_owned")){
+                                    carNumberList.add(carNumber);
+                                }
+                                break;
+                            }
+                        }
+                    }
+                });
+
+        db.collection("Car")
+                .get()
+                .addOnCompleteListener(task -> {
+                    boolean isLogin  = false;
+                    if(task.isSuccessful()){
+                        for(QueryDocumentSnapshot document : task.getResult()){
+                            if(carNumberList.contains(document.getId())){
+                                String name = (String)document.getData().get("name");
+                                String manufacture = (String)document.getData().get("manufacture");
+                                String number = (String)document.getData().get("number");
+                                String color = (String)document.getData().get("color");
+                                String type = (String)document.getData().get("type");
+                                int price = ((Long)document.getData().get("price")).intValue();
+                                int capacity = ((Long)document.getData().get("capacity")).intValue();
+                                int distanceDriven = ((Long)document.getData().get("distanceDriven")).intValue();
+                                int year = ((Long)document.getData().get("year")).intValue();
+                                String fuel = (String)document.getData().get("fuel");
+                                boolean isAccident = (boolean)document.getData().get("isAccident");
+                                boolean isTuned = (boolean)document.getData().get("isTuned");
+
+                                Car tmpCar = new Car(name, manufacture, number, color, type, price, capacity, distanceDriven, year, fuel, isAccident, isTuned);
+
+                                if(isAccident){
+                                    for(Accident accident : (ArrayList<Accident>)document.getData().get("accidentData")){
+                                        tmpCar.addAccident(accident);
+                                    }
+                                }
+
+                                if(isTuned){
+                                    for(Tune tune : (ArrayList<Tune>)document.getData().get("tuneData")){
+                                        tmpCar.addTune(tune);
+                                    }
+                                }
+
+                                user.addCar(tmpCar);
+                            }
+                        }
+                    }
+                });
+
         user.setIsSeller(isSeller);
         user.setName(userName);
 
